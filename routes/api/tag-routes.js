c43131fae2ace765758e0af5476c48fa6e47efdd
Brent B <brentbaughan1@gmail.com>
2021-05-15 16:11:53 -0400

    corrected similar include error generating SequelizeEagerLoadingError in each /:id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Tag }, { model: Product }],
+      include: [{ model: Product }],
     });
     if (!tagData) {
       res.status(404).json({ message: "No tag found with that id!" });
